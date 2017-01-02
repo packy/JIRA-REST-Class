384209cf7c9ac146fca9bcb63ff7dd2cc92fa7ac
Packy Anderson <packy@cpan.org>
2017-01-02 14:06:44 -0500

    Bump to 0.07 in source

--- lib/JIRA/REST/Class/Factory.pm
+++ lib/JIRA/REST/Class/Factory.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = '0.05';
+our $VERSION = '0.07';
 our $SOURCE = 'CPAN';
 $SOURCE = 'GitHub';  # COMMENT
 # the line above will be commented out by Dist::Zilla
