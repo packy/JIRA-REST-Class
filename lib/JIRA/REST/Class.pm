22a3de2971c4ec1994615bcaedb453cac4147514
Packy Anderson <packy@cpan.org>
2017-01-04 00:55:43 -0500

    Modify testing to not fail with JIRA::REST 0.014 and earlier.

--- lib/JIRA/REST/Class.pm
+++ lib/JIRA/REST/Class.pm
@@ -119,7 +119,7 @@ throws_ok(
 
 throws_ok(
     sub {
-        JIRA::REST::Class->new('https://jira.example.com/');
+        JIRA::REST::Class->new(TestServer_url());
     },
     qr/No credentials found/,
     q{JIRA::REST::Class->new with just url tries to find credentials},
@@ -127,8 +127,7 @@ throws_ok(
 
 lives_ok(
     sub {
-        JIRA::REST::Class->new('https://jira.example.com/',
-                               'user', 'pass');
+        JIRA::REST::Class->new(TestServer_url(), 'user', 'pass');
     },
     q{JIRA::REST::Class->new with url, username, and password does't croak!},
 );
