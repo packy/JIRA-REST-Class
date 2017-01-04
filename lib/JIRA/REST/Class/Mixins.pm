b24ea6efb9124e38303b3160c7331daed14eda9c
Packy Anderson <packy@cpan.org>
2017-01-04 01:32:22 -0500

    Bump to 0.10 in source

--- lib/JIRA/REST/Class/Mixins.pm
+++ lib/JIRA/REST/Class/Mixins.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 our $SOURCE = 'CPAN';
 $SOURCE = 'GitHub';  # COMMENT
 # the line above will be commented out by Dist::Zilla
