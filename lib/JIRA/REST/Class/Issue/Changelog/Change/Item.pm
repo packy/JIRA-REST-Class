ead19751750080d0a4c9ba0d6bdceab1f80e8a3f
Packy Anderson <packy@cpan.org>
2017-05-29 08:57:31 -0400

    Bump sources to version 0.12, and add code to recover the password from the Authorization header if we haven't cached it.
    Completes fix for https://github.com/packy/JIRA-REST-Class/issues/6

--- lib/JIRA/REST/Class/Issue/Changelog/Change/Item.pm
+++ lib/JIRA/REST/Class/Issue/Changelog/Change/Item.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.010;
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 our $SOURCE = 'CPAN';
 $SOURCE = 'GitHub';  # COMMENT
 # the line above will be commented out by Dist::Zilla
